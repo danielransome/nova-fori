0aa288bdaf22e89d95d5aa417e5e1dfa011074c9
ransomedesign <info@ransomedesign.co.uk>
2023-02-12 10:19:40 +0000

    Planning upcoming tests for story 1

--- src/components/to-do-list/ToDoList.spec.tsx
+++ src/components/to-do-list/ToDoList.spec.tsx
@@ -4,12 +4,24 @@ import { describe, expect, it } from 'vitest'
 import ToDoList from './ToDoList'
 
 describe('The ToDo list container', () => {
-  describe('the list should initially be empty', () => {
+  describe('when there are no tasks to display', () => {
     it('should have an empty list', () => {
       render(<ToDoList />)
 
       expect(screen.getByTestId('to-do-list').children.length).toBe(0)
     })
+  })
+
+  describe('when tasks exist', () => {
+    it('should display all tasks', () => {
+      render(<ToDoList />)
+    })
+
+    it.todo('should display tasks in two groups: completed and pending')
+
+    it.todo('should display tasks in the order they were added')
+
+    it.todo('show diplay a text description for each task')
   })
 })
 
