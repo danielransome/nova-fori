124aafe371df0a51e12b2d029f49838f675961ba
ransomedesign <info@ransomedesign.co.uk>
2023-02-13 13:22:08 +0000

    Adding toggle implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,53 @@ import React from 'react'
 import { useState } from 'react'
 
 import './App.css'
-import ToDoList, { ToDoListProps } from './components/to-do-list/ToDoList'
+import { TaskProps } from './components/to-do-list/Task'
+import ToDoList from './components/to-do-list/ToDoList'
+import { generateTaskId } from './utils/taskUtils'
+
+function createNewTask(
+  description: TaskProps['description']
+): Omit<TaskProps, 'onToggleCompleted'> {
+  const id = generateTaskId()
+
+  return {
+    id,
+    description,
+    completed: false,
+  }
+}
 
 function App() {
-  const [taskList, setTaskList] = useState<ToDoListProps['tasks']>([])
+  const [taskList, setTaskList] = useState<
+    Omit<TaskProps, 'onToggleCompleted'>[]
+  >([])
 
   const onSubmitNewTask = (description: string) => {
-    setTaskList((tasks) => [
-      ...tasks,
-      {
-        description,
-        completed: false,
-      },
-    ])
+    setTaskList((tasks) => [...tasks, createNewTask(description)])
+  }
+
+  const onToggleCompleted = (id: string) => {
+    setTaskList((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed,
+          }
+        }
+
+        return task
+      })
+    )
   }
 
   return (
     <div className="App">
-      <ToDoList tasks={taskList} onSubmitNewTask={onSubmitNewTask} />
+      <ToDoList
+        tasks={taskList}
+        onSubmitNewTask={onSubmitNewTask}
+        onToggleCompleted={onToggleCompleted}
+      />
     </div>
   )
 }
