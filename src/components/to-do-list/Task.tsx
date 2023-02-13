725fbce612897a7858918ea7403b20826e5fbc17
ransomedesign <info@ransomedesign.co.uk>
2023-02-13 08:39:10 +0000

    Passed label test

--- src/components/to-do-list/Task.tsx
+++ src/components/to-do-list/Task.tsx
@@ -9,6 +9,10 @@ const Task: React.FC<TaskProps> = ({ description, completed }) => {
   return (
     <div>
       <h3>{description}</h3>
+
+      <label>
+        Completed <input type="checkbox" />
+      </label>
     </div>
   )
 }
